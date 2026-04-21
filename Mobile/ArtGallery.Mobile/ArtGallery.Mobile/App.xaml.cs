46f420e66b56b4b335fbd30c3f73f0256c87bf34
AnnaMorozovac <morozova.anna@chnu.edu.ua>
2026-04-21 17:44:51 +0300

    [laba4] added dotnet maui project

---
+++ Mobile/ArtGallery.Mobile/ArtGallery.Mobile/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace ArtGallery.Mobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
---
+++ Mobile/ArtGallery.Mobile/ArtGallery.Mobile/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ArtGallery.Mobile
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
